922a3983cf6a257e5b5194f31fd6f485e751700a
kazburrel <oegbosionu@gmail.com>
2023-12-01 17:35:25 +0100

    Setting gate and middleware

--- app/Http/Middleware/GateDefineMiddleware.php
+++ app/Http/Middleware/GateDefineMiddleware.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Permission;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class GateDefineMiddleware
@@ -15,6 +17,15 @@ class GateDefineMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (auth()->check()) {
+            $permissions = Permission::whereHas('roles', function ($query) {
+                $query->where('roles.id', auth()->user()->role_id);
+            })->get();
+
+            foreach ($permissions as $permission) {
+                Gate::define($permission->name, fn () => true);
+            }
+        }
         return $next($request);
     }
 }
