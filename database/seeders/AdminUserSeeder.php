1b5e8b3206121c7ed1b47ef301b2ea2c0b7c1c4d
kazburrel <oegbosionu@gmail.com>
2023-12-01 14:45:07 +0100

    roles and permission

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_id = Role::where('name', 'Administrator')->first();
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'superadmin@booking.com',
+            'password' => bcrypt('SuperSecretPassword'),
+            'email_verified_at' => now(),
+            'role_id' => $role_id->id, // Administrator
+        ]);
+    }
+}
