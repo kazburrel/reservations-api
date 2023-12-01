2ea60aaa26eab4a0529c565f70ef24f3ccbe9caa
kazburrel <oegbosionu@gmail.com>
2023-12-01 15:48:08 +0100

    roles and permission

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,8 +9,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
+    const ROLE_ADMINISTRATOR = '9abec43f-5917-46e6-a5f4-923a538ea12a';
+    const ROLE_OWNER = '9abec43f-5f2b-49a5-b1c0-1387e7168ba9';
+    const ROLE_USER = '9abec43f-5f78-4be7-9582-360a418c3420';
+
     use HasFactory, HasUuids, SoftDeletes;
     protected $fillable = ['name'];
+
 
     public function permissions()
     {
