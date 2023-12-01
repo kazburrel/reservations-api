badb9eeb8ace02a55ed8ba18a89c4e766a6f2179
kazburrel <oegbosionu@gmail.com>
2023-12-01 17:19:18 +0100

    Setting gate and middleware

--- app/Http/Controllers/Owner/PropertyController.php
+++ app/Http/Controllers/Owner/PropertyController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    //
+    public function index()
+    {
+        $this->authorize('properties-manage');
+        // Will implement property management later
+        return response()->json(['success' => true]);
+    }
 }
--- app/Http/Controllers/User/BookingController.php
+++ app/Http/Controllers/User/BookingController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
+    public function index()
+    {
+        $this->authorize('bookings-manage');
+        // Will implement booking management later
+        return response()->json(['success' => true]);
+ 
+    }
 }
