afa70c07b274557e73101740fd901f29c04b710f
kazburrel <oegbosionu@gmail.com>
2023-12-05 15:51:36 +0100

    geo coordinates

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Ramsey\Uuid\Nonstandard\Uuid;
 
 class CitySeeder extends Seeder
 {
@@ -12,6 +14,19 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $id = Uuid::uuid4();
+        City::create([
+            'country_id' => $id,
+            'name' => 'New York',
+            'lat' => 40.712776,
+            'long' => -74.005974,
+        ]);
+
+        City::create([
+            'country_id' => $id,
+            'name' => 'London',
+            'lat' => 51.507351,
+            'long' => -0.127758,
+        ]);
     }
 }
