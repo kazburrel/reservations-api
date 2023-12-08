2c4c8d58617736b0e3faeef5136b48a612a9bfa9
kazburrel <oegbosionu@gmail.com>
2023-12-08 11:38:06 +0100

    search feature

--- app/Http/Controllers/Public/PropertySearchController.php
+++ app/Http/Controllers/Public/PropertySearchController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertySearchController extends Controller
 {
-    //
+    public function __invoke(Request $request)
+    {
+        return Property::with('city')
+            ->when($request->has('city'), function ($query) use ($request) {
+                $query->where('city_id', $request->city);
+            })
+            ->when($request->country, function($query) use ($request) {
+                $query->whereHas('city', fn($q) => $q->where('country_id', $request->country));
+            })
+            ->get();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Owner\PropertyController;
+use App\Http\Controllers\Public\PropertySearchController;
 use App\Http\Controllers\User\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,8 +24,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('auth/register', RegisterController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
-    // No owner/user grouping, for now, will do it later with more routes
-    Route::get('owner/properties', [PropertyController::class, 'index']);
-    Route::get('user/bookings', [BookingController::class, 'index']);
-    Route::post('owner/properties', [PropertyController::class, 'store']);
+    Route::prefix('owner')->group(function () {
+        Route::get('properties',  [PropertyController::class, 'index']);
+        Route::post('properties', [PropertyController::class, 'store']);
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::get('bookings', [BookingController::class, 'index']);
+    });
 });
+
+Route::get('search', PropertySearchController::class);
