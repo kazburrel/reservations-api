b9e35b2ba7164942f23e3243eabb73ac23e73b20
kazburrel <oegbosionu@gmail.com>
2023-12-12 13:23:00 +0100

    apartment feature

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Room extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'apartment_id',
+        'room_type_id',
+        'name',
+    ];
+
+    public function room_type()
+    {
+        return $this->belongsTo(RoomType::class);
+    }
 }
