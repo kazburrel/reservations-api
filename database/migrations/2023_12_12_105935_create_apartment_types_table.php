adcd5e7832159bb26a930d1a2ae12e138353cf6f
kazburrel <oegbosionu@gmail.com>
2023-12-12 12:05:36 +0100

    apartment feature

--- database/migrations/2023_12_12_105935_create_apartment_types_table.php
+++ database/migrations/2023_12_12_105935_create_apartment_types_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ApartmentType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,9 +13,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('apartment_types', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name');
             $table->timestamps();
         });
+     
+        ApartmentType::create(['name' => 'Entire apartment']);
+        ApartmentType::create(['name' => 'Entire studio']);
+        ApartmentType::create(['name' => 'Private suite']);
     }
 
     /**
